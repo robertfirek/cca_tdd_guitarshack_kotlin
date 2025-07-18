dc18f52b7efe06f611f4659daa3ebfd1f802009d
Robert Firek <robert@ikigaidigital.io>
2025-07-18 13:37:03 +0100

    [refactoring to change the design] Parallely introduce change in stock - part 2 -> make the feature robust

--- src/main/kotlin/com/guitarshack/stock/Stock.kt
+++ src/main/kotlin/com/guitarshack/stock/Stock.kt
@@ -4,5 +4,5 @@ import com.guitarshack.OrderItem
 
 class Stock(private val orderItems: List<OrderItem> = listOf(), private val products: Map<String, UInt> = emptyMap()) {
     fun numberOfAvailableItems(orderItem: OrderItem): Int = orderItems.count { it.product == orderItem.product }
-    fun hasSufficientQuantityOnStock(product: String, desiredQuantity: UInt) = products[product] == desiredQuantity
+    fun hasSufficientQuantityOnStock(product: String, desiredQuantity: UInt) = desiredQuantity != 0u && (products[product]?:0u) >= desiredQuantity
 }
--- src/test/kotlin/com/guitarshack/StockTest.kt
+++ src/test/kotlin/com/guitarshack/StockTest.kt
@@ -2,8 +2,10 @@ package com.guitarshack
 
 
 import com.guitarshack.stock.Stock
-import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import java.util.UUID
 import kotlin.test.assertEquals
 
@@ -20,14 +22,73 @@ class StockTest {
         assertEquals(anotherItemOnStock.size, stock.numberOfAvailableItems(anotherOrderItem))
     }
 
-    @Test
-    fun `should inform if it has sufficient quantity product`() {
-        val product = "Product on the stock"
-        val quantityOnStock = 3u
-        val desiredQuantity = 3u
-        val stock = Stock(products = mapOf(product to quantityOnStock))
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("hasSufficientQuantityOnStockScenarios")
+    fun `should inform if it has sufficient quantity product`(
+        scenario: String,
+        productsOnStock: Map<String, UInt>,
+        desiredQuantity: Int,
+        expectedHasSufficientQuantityAnswer: Boolean
+    ) {
+        val stock = Stock(products = productsOnStock)
 
-        assertTrue(stock.hasSufficientQuantityOnStock(product, desiredQuantity))
+        assertEquals(
+            expectedHasSufficientQuantityAnswer,
+            stock.hasSufficientQuantityOnStock(PRODUCT, desiredQuantity.toUInt())
+        )
+    }
+
+    companion object {
+        const val PRODUCT = "Product on the stock"
+        const val ANOTHER_PRODUCT = "Another product on the stock"
+        val QUANTITY_ON_STOCK = 3u
+        const val DESIRED_QUANTITY = 3
+
+        @JvmStatic
+        fun hasSufficientQuantityOnStockScenarios(): List<Arguments?> {
+            return listOf(
+                Arguments.of(
+                    "desired quantity is the same as the quantity on the stock",
+                    mapOf(PRODUCT to QUANTITY_ON_STOCK),
+                    DESIRED_QUANTITY,
+                    true
+                ),
+                Arguments.of(
+                    "desired quantity is lower than the quantity on the stock",
+                    mapOf(PRODUCT to QUANTITY_ON_STOCK),
+                    DESIRED_QUANTITY - 1,
+                    true
+                ),
+                Arguments.of(
+                    "the quantity of product on the stock is zero",
+                    mapOf(PRODUCT to 0u),
+                    DESIRED_QUANTITY,
+                    false
+                ),
+                Arguments.of(
+                    "the product is not available in the stock",
+                    emptyMap<String, UInt>(),
+                    DESIRED_QUANTITY,
+                    false
+                ),
+                Arguments.of(
+                    "desired quantity zero",
+                    mapOf(PRODUCT to QUANTITY_ON_STOCK),
+                    0,
+                    false
+                ),
+                Arguments.of(
+                    "multiple products are on the stock",
+                    mapOf(
+                        PRODUCT to QUANTITY_ON_STOCK,
+                        ANOTHER_PRODUCT to DESIRED_QUANTITY - 1
+                    ),
+                    DESIRED_QUANTITY,
+                    true
+                ),
+            )
+        }
+
     }
 }
 
